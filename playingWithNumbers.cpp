36bc3978c8235dd854dcd0e3f6d5dc3b92a1d5fc
Frieda Jacobson <kajakali@hotmail.com>
2020-07-24 21:18:15 -0500

    initializing variables and standard input/output

---
+++ playingWithNumbers.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+
+int main() {
+    int size ; //normally initialize variables, like this: int size{5}; if you don't they might be full of random numbers
+    std::cout << "the size right now is: " << size;
+    std::cout << "enter a number please: ";
+    std::cin >> size;
+    std::cout << "size is :" << '\n' << size << std::endl;
+    return 0;
+} 
